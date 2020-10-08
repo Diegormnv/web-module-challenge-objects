2308722de25b12f5a8210d8c92dc05e3f30ef197
Diego Roman <diegormnv209@gmail.com>
2020-10-08 14:01:12 -0700

    Done all seven problems

--- index.js
+++ index.js
@@ -82,8 +82,10 @@ Using the reviews array above do the following:
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-
-
+function addReview(arr, name, rating, feedback){
+  arr.push({name, rating, feedback});
+  return arr;
+}
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Reyna's feedback is missing! Use what you know to do the following:
@@ -91,9 +93,12 @@ Reyna's feedback is missing! Use what you know to do the following:
   2. log the reviews array to the console to check your work
 */
 
+function addFeedback(arr, index, feedback){
+  arr[index].feedback = feedback;
+  return arr;
+}
 
-
-
+console.log(addFeedback(reviews, 7, "this place is chill with really cool people, great for getting work done on weekdays"))
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Write a function to return a review based on the index of the review in the array.
@@ -105,8 +110,9 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(arr, index) {
+  const rev = arr[index]
+  return `${rev.name} gave the restaurant a ${rev.rating} star review, and their feedback was: ${rev.feedback}`
 }
 
 
@@ -123,9 +129,12 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(arr) {
+  const review = arr[arr.length - 1];
+
+  return `${review.name} gave the restaurant a ${review.rating} star review, and their feedback was: ${review.feedback}`
 } 
+
 
 
 
